452078b7cb6069647936175704157c904850bd0b
JueLance <zzc2626@163.com>
2017-04-02 15:01:46 +0800

    添加一个使用常量和只读变量的例子

---
+++ CLRviaCSharp/ConstAndReadOnlyValue/ConsoleApplication1/Program.cs
@@ -0,0 +1,20 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Class1.Str为常量，
+            //在编译时就已经被编译到程序集中
+            Console.WriteLine(Class1.Str);
+
+        }
+    }
+}
