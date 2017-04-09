d4ce8de4a966c000578bc0ee9fd171a1631dfe5d
JueLance <zzc2626@163.com>
2017-04-09 16:16:21 +0800

    添加单元测试的例子

---
+++ Redis/RedisUnitTest/WebApplication1/Models/BlogPostComment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class BlogPostComment
+    {
+        public string Content { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
