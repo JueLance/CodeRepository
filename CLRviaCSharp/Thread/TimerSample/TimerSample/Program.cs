81524fb9a8f844faf87b56a9f4a89333a6c7b132
JueLance <zzc2626@163.com>
2017-04-24 17:03:03 +0800

    修改了注释

--- CLRviaCSharp/Thread/TimerSample/TimerSample/Program.cs
+++ CLRviaCSharp/Thread/TimerSample/TimerSample/Program.cs
@@ -30,12 +30,12 @@ namespace TimerSample
             /*
              
             在内部，线程池为所有Timer对象只使用了一个线程。这个线程知道下一个Timer对象在什么时候到期(计时器还有多久触发)
-             下一个Timer对象到期时，线程就会唤醒，在内部，调用ThreadPool.QueueUserWorkItem，将一个工作项添加到线程池的队列中
-             使你的回调方法得到调用。
+            下一个Timer对象到期时，线程就会唤醒，在内部，调用ThreadPool.QueueUserWorkItem，将一个工作项添加到线程池的队列中
+            使你的回调方法得到调用。
 
-            如果回调方法的执行时间很长，计时器可能（在上个回调还没有完成的时候）再次触发。这可能造成多个线程池线程同时执行你的回调方法
-            。为了解决这个问题，作者的建议：构造Timer时，为period参数指定Timeout.Infinite。这样计时器就只出发一次。然后在你的回调方法中，调用change
-            方法来指定一个新的dueTime，并再次为period参数指定Timeout.Infinite。
+            如果回调方法的执行时间很长，计时器可能（在上个回调还没有完成的时候）再次触发。这可能造成多个线程池线程同时执行你的回调方法。
+            为了解决这个问题，作者的建议：构造Timer时，为period参数指定Timeout.Infinite。这样计时器就只出发一次。然后在你的回调方法中，
+            调用change方法来指定一个新的dueTime，并再次为period参数指定Timeout.Infinite。
              
              
              */
