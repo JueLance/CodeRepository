e114dfedf89b3637c2434e324c317c453c3d84e3
zzc2626@163.com <zzc2626@163.com>
2017-05-14 19:22:28 +0800

    添加C++和C# CLR的例子

---
+++ CLRviaCSharp/StaticClass/StaticClass/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Person
+    {
+        public Person()
+        {
+            Console.WriteLine("ins ctor"); ;
+        }
+        static Person()
+        {
+            Console.WriteLine("Type ctor");
+        }
+    }
+}
---
+++ CLRviaCSharp/StaticClass/StaticClass/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+            Person p1 = new Person();
+            Console.ReadKey();
+
+        }
+    }
+}
