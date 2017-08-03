e863e5a71ccf67399afb74a5947426bf2c1e6387
JueLance <zzc2626@163.com>
2017-08-03 12:45:04 +0800

    添加一个CLR寄宿的例子（在非托管程序中执行托管程序）

---
+++ CLRviaCSharp/CLRHost/SampleManagedApp/SampleManagedApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleManagedApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static int Test(string s)
+        {
+            Console.WriteLine(s + " - " + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + " from C# Console Application.");
+            return DateTime.Now.Second;
+        }
+    }
+
+}
