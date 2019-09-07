8db0a9801115eb256097de9ecde003f32cc6de0b
Rocky-Home <zzc2626@163.com>
2019-09-07 19:01:18 +0800

    添加注释

--- Design_Patterns/StatePattern/StatePattern/Program.cs
+++ Design_Patterns/StatePattern/StatePattern/Program.cs
@@ -10,10 +10,10 @@ namespace StatePattern
         static void Main(string[] args)
         {
             Context c = new Context(new ConcreteStateA());
-            c.Request();
-            c.Request();
-            c.Request();
-            c.Request();
+            c.Request();//A本身的事情处理完成以后，将State修改成B
+            c.Request();//继续调用后，由B来处理事情。B处理完以后，将State修改成C
+            c.Request();//继续调用后，由C来处理事情。C处理完以后，将State修改成null
+            c.Request();//由于C将State修改成null，因此这里只是一个空调用
             c.Request();
 
             Console.Read();
